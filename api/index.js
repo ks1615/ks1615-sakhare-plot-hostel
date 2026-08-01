ad44d41dd801b8809d13fd307ff1fa46c5ff4b31
ks1615 <kskrushna1615@gmail.com>
2026-08-01 16:10:17 +0530

    fix(vercel): preserve original req.url in serverless endpoints for Vercel catch-all routing

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const handleRequest = require('../server');
 
 module.exports = async (req, res) => {
+  if (req.headers && req.headers['x-matched-path']) {
+    req.url = req.headers['x-matched-path'];
+  }
   return handleRequest(req, res);
 };
